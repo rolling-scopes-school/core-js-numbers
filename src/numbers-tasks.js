4e147673d81c860e27006092b64c93019d6d36c2
Dzmitry Yushkevich <ip.yshkevich@gmail.com>
2023-12-10 22:00:29 +0300

    feast: add function getinearquationoot

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -86,8 +86,8 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  return -b / a;
 }
 
 /**
