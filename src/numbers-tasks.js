735fbb2804bb7d3d2c21978e0ce8446a13fd0fa6
Dzmitry Yushkevich <ip.yshkevich@gmail.com>
2023-12-11 16:55:14 +0300

    feast: add function toNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -233,8 +233,14 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  let number = Number(value);
+  if (Number.isNaN(number)) {
+    number = def;
+  } else {
+    number = Number(value);
+  }
+  return number;
 }
 
 /**
