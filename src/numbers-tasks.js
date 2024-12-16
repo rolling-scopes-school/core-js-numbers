3c224007291ce7800d77d2ca93c65c22d83ce957
DariaAbramovich <kamaradaria@gmail.com>
2024-12-16 23:21:32 +0300

    feat: add task with   a string representation of a number in a specified base (radix).

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -328,8 +328,8 @@ function getSine(num) {
  * 255, 16 => 'ff'
  * 2, 2    => '10'
  */
-function numberToStringInBase(/* number, base */) {
-  throw new Error('Not implemented');
+function numberToStringInBase(number, base) {
+  return number.toString(base);
 }
 
 /**
